551bb3fe0eff862a083d0566db269e51e0701a01
JoseGeek78 <josemariarodriguez78@gmail.com>
2023-11-11 08:35:03 +0100

    Update

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -8,6 +8,7 @@ function ListaDeTareas() {
   const [tareas, setTareas] = useState([]);
 
   const agregarTarea = tarea => {
+    console.log(tarea);
     if (tarea.texto.trim()) {
       tarea.texto = tarea.texto.trim();
       const tareasActualizadas = [tarea, ...tareas];
@@ -22,6 +23,8 @@ function ListaDeTareas() {
       {
         tareas.map((tarea) =>
           <Tarea 
+            key={tarea.id}
+            id={tarea.id}
             texto={tarea.texto}
             completada={tarea.completada}
           />
